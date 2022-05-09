3434e727f80561fbbc09c2b190dc6d9f0a2c805f
Zufar Sharipov <zerosharp291@gmail.com>
2022-05-09 08:25:05 +0400

    fixit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import React, {useState} from 'react';
 
-function App () {
+function App (props) {
+  const [name, setName] = useState('James Bond')
+
+  changeName () {
+    let names = props.names;
+    let name_index = Math.floor(Math.random() * names.length);
+    setName(names[name_index]);
+  }
+
   return (
     <div>
-        <p>Hello World!</p>
+        <p>My name is {name}</p>
+        <button onClick={changeName}>Change name</button>
     </div>
   )
 }
